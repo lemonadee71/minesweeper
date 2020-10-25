dc33971c43d716e6e006ab311e048eb6cb6adf56
Shin Andrei Riego <riegoshinandrei@gmail.com>
2020-10-25 20:59:19 +0800

    Nearby bombs are now counted

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
 let gridSize = 16;
 
+const hasClass = (element, cls) => {
+  return element.className.includes(cls);
+}
+
 const addBombs = () => {
   let noOfBombs = Math.floor(gridSize**2 * 0.3)
   for (let i = 0; i < noOfBombs; i++) {
     let row = Math.floor(Math.random() * gridSize) + 1,
       col = Math.floor(Math.random() * gridSize) + 1,
-      cell = document.querySelector(`.cell[data-row-col="${row}-${col}"]`)
+      bombCell = document.querySelector(`.cell[data-row-col="${row}-${col}"]`)
     
-    cell.classList.add('bomb')
-    cell.addEventListener('click', () => {
-      
+    bombCell.classList.toggle('bomb')
+    bombCell.addEventListener('click', () => {
+      alert('You clicked on a bomb. Game over!')
+      clearGrid()
     })
   }
+}
+
+const checkSurroundings = (e) => {
+  let [row, col] = e.target.getAttribute('data-row-col').split('-'),
+    noOfBombs = 0
+
+  e.target.style.backgroundColor = 'royalblue'
+
+  for (let i = row - 1, x = i + 3; i < x; i++) {
+    for (let j = col - 1, y = j + 3; j < y; j++) {
+      if ((i >= 0 && i < gridSize) && (j >= 0 && j < gridSize)) {
+        let nearbyCell = document.querySelector(`.cell[data-row-col="${i}-${j}"]`)
+        
+        if (hasClass(nearbyCell, 'bomb')) {
+          noOfBombs++
+        } else {
+          //nearbyCell.style.backgroundColor = 'royalblue'
+        }
+      }
+    }
+  }
+  
+  return noOfBombs
 }
 
 const drawGrid = (size) => {
@@ -23,6 +51,15 @@ const drawGrid = (size) => {
       let cell = document.createElement('div')
       cell.classList.add('cell')
       cell.setAttribute('data-row-col', `${i}-${j}`)
+
+      cell.addEventListener('click', (e) => {
+        let bombsNearby = checkSurroundings(e)
+        let text = document.createElement('p')
+        text.textContent = `${bombsNearby}`
+        cell.appendChild(text);
+      })
+
+      
       grid.appendChild(cell);
     }
     /* For etch-a-sketch
@@ -41,20 +78,20 @@ const clearGrid = () => {
   while (grid.firstChild) {
     grid.removeChild(grid.lastChild)
   }
+  drawGrid(gridSize);
 }
 
 let button = document.getElementById('submit')
 button.addEventListener('click', () => {
   let value = document.getElementById('grid-size').value
 
-  if (value > 100) {
-    alert('Max grid size is 100')
+  if (value > 50) {
+    alert('Max grid size is 50')
     return;
   }
 
   gridSize = parseInt(value);
   clearGrid();
-  drawGrid(gridSize);
 })
 
 document.addEventListener('onload', drawGrid(gridSize))
